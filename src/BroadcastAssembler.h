bbdb1d472bf7a1751e5698a7b063b0a721d4fe47
Alejandro Francisco Queiruga <afq@berkeley.edu>
2015-03-25 14:47:26 -0700

    Tricking instant to change the hash and recompile.

--- src/BroadcastAssembler.h
+++ src/BroadcastAssembler.h
@@ -57,7 +57,7 @@ namespace dolfin
 		       const GenericDofMap & mappeddof);
     void assemble_exterior_facets(const Form &a,
 				  const GenericDofMap & mappeddof,
-				  std::shared_ptr<const MeshFunction<std::size_t> > domainss);
+				  std::shared_ptr<const MeshFunction<std::size_t> > domains);
     void assemble_cell_pair(const Form &a,
 			    const Mesh & meshA, const GenericDofMap & mdofA,
 			    const Form &b,
